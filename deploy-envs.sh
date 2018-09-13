b0366b59e95018ad57880f9dbb87deeb399004bb
Shibly <shibly77@outlook.com>
2018-09-13 17:14:48 +0600

    test

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -8,6 +8,6 @@ export AWS_DEFAULT_REGION=us-east-2
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=18.223.191.192
 
-# set any sensitive information in travis-ci encrypted project settings:
+# set any sensitive information in travis-ci encrypted project settings: i mo 
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 # optional: SERVICESTACK_LICENSE
