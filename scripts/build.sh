1467284d154cfc23c5598587850fb8f5b7c5a40f
Shibly <shibly77@outlook.com>
2018-09-16 14:50:59 +0600

    finlaalala

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,9 @@ export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazo
 
 # Build process
 
-DOCKER_TAG='latest'
 
-docker build -f ../src/Services/Agent/Agent.API/Dockerfile -t agent-api ../src/Services/Agent/Agent.API --no-cache
-docker tag agent-api $AWS_ECS_REPO_DOMAIN/fibonapi
+docker build -f ../src/Services/Agent/Agent.API/Dockerfile -t $IMAGE_NAME ../src/Services/Agent/Agent.API --no-cache
+
+docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
+
 
